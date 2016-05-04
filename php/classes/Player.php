0e6b2e3f9fa009611ee0f9f2422eb70f64778173
Julian Schönbächler <info@creative-forge.ch>
2016-05-04 21:48:45 +0200

    Create basic Player class.

--- php/classes/Player.php
+++ php/classes/Player.php
@@ -1,10 +1,51 @@
 <?php
+	namespace CYOA_Engine;
 	
-	interface iPlayer
+	final class Player
 	{
-		public function saveData();
-		public function loadData();
-		//
+		// Properties
+		private $id;				// ID of the player
+		private $history;			// Displayed history, the player went through
+		private $memory;			// Associative Array / Bool hash table of actions, the character remembers
+		private $experience;		// Associative Array / Bool hash table of actions, the player has done
+		private $finished;			// Finished game? / Ready to start game?
+		private $points;			// Counting points...
+		private $avatar;			// Associative Array of images which are used for the avatar picture
+		public $name;				// Name of the player
+		
+		// Class constructor
+		public function __construct($id)
+		{
+			$this->id = $id;
+		}
+		
+		// Load player data
+		public function loadData()
+		{
+			
+		}
+		
+		// Save player data
+		public function saveData()
+		{
+			
+		}
+		
+		// Create new player data
+		public function newData()
+		{
+			
+		}
 	}
+	
+	// Player
+	// ======
+	// memory (bool hash-table) synchron
+	// -- experienced (bool hash-table)
+	// name
+	// id
+	// finished
+	// points
+	// 
 	
 ?>
