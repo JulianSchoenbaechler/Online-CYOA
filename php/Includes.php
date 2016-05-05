3ef9943ccb90b66d1feff0e7e345e36261534d0e
Julian Schönbächler <info@creative-forge.ch>
2016-05-05 23:46:57 +0200

    Finally adding this freaking file!

---
+++ php/Includes.php
@@ -0,0 +1,26 @@
+<?php
+/**
+*
+* This file is part of the 'Online Choose your own Adventure' MultiOil (working title).
+*
+* University of the Arts Zurich (Switzerland)
+* Game Design 2016
+* Task: Writing and producing an Online CYOA, including own illustrations.
+*
+* @copyright (c) University of the Arts Zurich
+*
+* Author(s):
+* Arno Justus
+* Julian Schönbächler
+* Marcel Arioli
+* Michael von Ah
+*
+*/
+	namespace CYOA_Engine;
+	
+	// Includes
+	require_once '../Constants.php';
+	require_once 'History.php';
+	require_once 'DatabaseController.php';
+
+?>
