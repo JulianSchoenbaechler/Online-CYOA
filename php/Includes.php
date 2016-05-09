19f62f823e196838fb7d191f086670907525b28c
Julian Schönbächler <info@creative-forge.ch>
2016-05-09 17:03:07 +0200

    Adding 'SessionController' class to includes. Error reporting options on.

--- php/Includes.php
+++ php/Includes.php
@@ -18,10 +18,15 @@
 */
 	namespace CYOA_Engine;
 	
+	// Error reporting
+	error_reporting(1);
+	ini_set('display_errors', E_ALL);
+	
 	// Includes
 	require_once 'Constants.php';
 	require_once 'Database.php';
 	require_once 'classes/DatabaseController.php';
+	require_once 'classes/SessionController.php';
 	require_once 'classes/Player.php';
 	require_once 'classes/Story.php';
 
