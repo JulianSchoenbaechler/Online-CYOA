415873708c51c3cebe06b2a09064aaaca2de88bd
Julian Schönbächler <info@creative-forge.ch>
2016-05-15 22:14:15 +0200

    Set evaluation of story fragment into an own function.

--- javascript/gameplay.js
+++ javascript/gameplay.js
@@ -16,86 +16,56 @@
 *
 */
 
+// Evaluate received story fragment
+// Change template, texts and answers
+function evaluateFragment(fragment) {
+	
+	// Still logged in?
+	if(fragment != "logout") {
+		
+		// Load HTML template
+		$("body").load("template/" + fragment.template + ".html", function() {
+			
+			$("#title").html(/*fragment.title*/'<img src="https://upload.wikimedia.org/wikipedia/commons/c/c7/House_at_1329_Carroll_Ave.%2C_Los_Angeles_%28Charmed_House%29.JPG" />');
+			$("#text").html(fragment.text);
+			
+			// Delete existing answer links
+			$("#answers").html(' ');
+			
+			// Resolve new answers
+			alert(fragment.answers);
+			$.each($.parseJSON(fragment.answers), function(i, object) {
+				
+				$("#answers").append('<a href="#" onclick="goto(' + object.id + ')">' + object.answer + '</a><br />');
+				
+			});
+			
+			// Let page load...
+			$("#container").waitForImages(function() {
+				alert("test");
+			});
+			
+		});
+	
+	}
+	else
+	{
+		// Redirect to index page
+		window.location.assign("index.html");
+	}
+	
+}
+
 // Player has clicked an answer / option
 function goto(answerID) {
 	
-	$.post("php/game.php", { task: "answer", id: answerID.toString() }, function(fragment) {
-		
-		// Still logged in?
-		if(fragment != "logout") {
-			
-			// Load HTML template
-			$("body").load("template/" + fragment.template + ".html", function() {
-				
-				$("#title").html(fragment.title);
-				$("#text").html(fragment.text);
-				
-				// Delete existing answer links
-				$("#answers").html(' ');
-				
-				// Resolve new answers
-				$.each($.parseJSON(fragment.answers), function(i, object) {
-					
-					$("#answers").append('<a href="#" onclick="goto(' + object.id + ')">' + object.answer + '</a><br />');
-					
-				});
-				
-				// Let page load...
-				$("#container").waitForImages(function() {
-					alert("test");
-				});
-				
-			});
-		
-		}
-		else
-		{
-			// Redirect to index page
-			window.location.assign("index.html");
-		}
-		
-	}, "json");
+	$.post("php/game.php", { task: "answer", id: answerID.toString() }, evaluateFragment, "json");
 	
 }
 
 // When document ready
 $(document).ready(function() {
 	
-	$.post("php/game.php", { task: "reload" }, function(fragment) {
-		
-		// Still logged in?
-		if(fragment != "logout") {
-			
-			// Load HTML template
-			$("body").load("template/" + fragment.template + ".html", function() {
-				
-				$("#title").html(fragment.title);
-				$("#text").html(fragment.text);
-				
-				// Delete existing answer links
-				$("#answers").html(' ');
-				
-				// Resolve new answers
-				$.each($.parseJSON(fragment.answers), function(i, object) {
-					
-					$("#answers").append('<a href="#" onclick="goto(' + object.id + ')">' + object.answer + '</a><br />');
-					
-				});
-				
-				// Let page load...
-				$("#container").waitForImages(function() {
-					alert("test");
-				});
-				
-			});
-		
-		}
-		else
-		{
-			// Redirect to index page
-			window.location.assign("index.html");
-		}
-		
-	}, "json");
+	$.post("php/game.php", { task: "reload" }, evaluateFragment, "json");
 	
 });
